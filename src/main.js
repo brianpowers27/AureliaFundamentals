c068af6b01af6301adc05e6b80c45ede9b86ca76
Brian Powers <brianpowers27@gmail.com>
2017-03-22 11:55:29 -0400

    checking in individually

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import 'bootstrap';
 
 export function configure(aurelia) {
-	aurelia.use.standardConfiguration().developmentLogging();
+	aurelia.use.instance('apiRoot', 'http://localhost:27092/')
+	aurelia.use.globalResources('common/dateFormat');
+	aurelia.use
+	.standardConfiguration()
+	.developmentLogging()
+	.plugin('aurelia-dialog');
 	aurelia.start().then(a=>a.setRoot("shell"));
 }
